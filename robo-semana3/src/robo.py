3698c40918ad338c2898937c36ce33b2ea7e52ee
ItsVasconcellos <fernando.asv@uol.com.br>
2024-02-28 13:24:21 -0300

    feat: robot funcionalities

--- robo-semana3/src/robo.py
+++ robo-semana3/src/robo.py
@@ -1,23 +1,40 @@
 # Traz a ferramenta serial para apresentar quais portas estão disponíveis
 from serial.tools import list_ports
 import inquirer
-import pydobot
+import pydobot 
 
-# Listas as portas seriais disponíveis
-available_ports = list_ports.comports()
+class InteliArm(pydobot.Dobot):
+    def __init__(self, port=None, verbose=False):
+        super().__init__(port=port, verbose=verbose)
+    
+    def movej_to(self, x, y, z, r, wait=True):
+        super()._set_ptp_cmd(x, y, z, r, mode=pydobot.enums.PTPMode.MOVJ_XYZ, wait=wait)
 
-
-# Pede para o usuário escolher uma das portas disponíveis
-porta_escolhida = inquirer.prompt([
-    inquirer.List("porta", message="Escolha a porta serial", choices=[x.device for x in available_ports])
-])["porta"]
+    def movel_to(self, x, y, z, r, wait=True):
+        super()._set_ptp_cmd(x, y, z, r, mode=pydobot.enums.PTPMode.MOVL_XYZ, wait=wait)
 
 # Cria uma instância do robô
-robo = pydobot.Dobot(port=porta_escolhida, verbose=False)
+robo = InteliArm(port="COM4", verbose=False)
 
-# Pega a posição atual do robô
-posicao_atual = robo.pose()
-print(f"Posição atual: {posicao_atual}")
+robo.speed(150, 150)
 
+
+menu = inquirer.prompt([inquirer.List( 'funcionalidade',
+                message="Qual função você quer iniciar?",
+                choices=['Home', 'Ligar Ferramenta', 'Desligar ferramenta', 'Atual', 'Small', 'Micro'])])
+
+match menu["funcionalidade"]:
+    case "Home":
+        robo.movej_to(240.2, 0 , 150.5, 0, wait=True)
+    case "Ligar Ferramenta":
+        robo.suck(enable)
+    case "Desligar Ferramenta": 
+        robo.suction(enable)
+    case "Atual": 
+        # Pega a posição atual do robô
+        posicao_atual = robo.pose()
+        print(f"Posição atual: {posicao_atual}")    
+
+#robo.movej_to(240.2, 0, posicao_atual[2], posicao_atual[3],wait=True)
 # Fecha a conexão com o robô
 robo.close()
